4828496425599ca068feff09ba301a464d4ccf9f
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-10-01 22:10:19 +0000

    Use ES6’s for...of operator

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const marray = [];
+  for (const item of array) {
+    marray.push(appendString + item);
+  }
+
+  return marray;
+}
