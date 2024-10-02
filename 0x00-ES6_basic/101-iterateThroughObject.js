a8ac5edbde94a48616fa872c87609ae70fad1f00
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-10-02 14:52:03 +0000

    Iterating through objects

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,14 @@
+/* eslint-disable radix */
+export default function iterateThroughObject(reportWithIterator) {
+  let result = '';
+
+  for (const [idx, item] of Object.entries(reportWithIterator)) {
+    result += `${item}`;
+
+    if (parseInt(idx) !== reportWithIterator.length - 1) {
+      result += ' | ';
+    }
+  }
+
+  return result;
+}
