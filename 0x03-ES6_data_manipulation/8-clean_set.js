cf37f9029c7a573f094b4252a8efa62e5034f6fe
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-10-09 16:00:30 +0000

    Updated solution

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
+/* eslint-disable array-callback-return */
 export default function cleanSet(set, string) {
   if (string === undefined || string.length === 0) {
     return '';
   }
-  const result = Array.from(set)
-    .filter((value) => value.startsWith(string))
-    .map((value) => value.slice(string.length));
-
-  return result.join('-');
+  return [...set]
+    .filter((value) => (value !== undefined ? value.startsWith(string) : ''))
+    .map((value) => (value !== undefined ? value.slice(string.length) : ''))
+    .join('-');
 }
